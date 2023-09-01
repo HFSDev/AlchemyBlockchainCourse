1ccdd78fab3680cde4dfb7a207aa5267af73bfe8
Hugo Frederico Soares <hugofredericosoares@gmail.com>
2023-09-01 20:39:38 -0300

    Repo organization and second assignment from the course.

---
+++ GiftList/client/index.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
+const verifyProof = require('../utils/verifyProof');
+
+const serverUrl = 'http://localhost:1225';
+
+async function main() {
+  // TODO: how do we prove to the server we're on the nice list?
+// create the merkle tree for the whole nice list
+const merkleTree = new MerkleTree(niceList);
+const root = merkleTree.getRoot();
+// find the proof that norman block is in the list 
+const name = 'Norman Block';
+const index = niceList.findIndex(n => n === name);
+const proof = merkleTree.getProof(index);
+
+// verify proof against the Merkle Root
+// console.log( verifyProof(proof, name, root) ); // true, Norman Block is in the list!
+
+// TRY IT OUT: what happens if you try a name not in the list, or a fake proof?
+
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    name,
+    proof
+  });
+
+  console.log({ gift });
+}
+
+main();
